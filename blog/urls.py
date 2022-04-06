5386d7c87fbcb4de9e02f71c9a0b0b7b01a33437
Shafquet Naghmi <shafquetnaghmi@hotmail.com>
2022-04-06 17:12:01 +0530

    added views in blog

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
-
+form . import views
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('blogapp/',include('blogapp.urls')),
     path('',views.blogretrieve,name='blog'),
+
 ]
